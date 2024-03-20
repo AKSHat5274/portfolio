5f2ae0cb20aee5bc27a8063eb02f5af7dcdc68a5
AKSHat5274 <akshatsharma5274@gmail.com>
2024-03-21 04:21:40 +0530

    META

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,12 @@ const Navbar = () => {
     <header className="header">
       <Link
         href="/"
-        className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md "
+        className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md m-2 "
       >
         {/* <img src={logo} alt='logo' className='w-18 h-18 object-contain' /> */}
         <p className="blue-gradient_text">AS</p>
       </Link>
-      <nav className="flex text-lg gap-7 font-medium">
+      <nav className="flex text-lg font-medium m-2 gap-7">
         <Link
           href="/about"
           className={({ isActive }) =>
@@ -34,7 +34,8 @@ const Navbar = () => {
             <img
               src={social.iconUrl.src}
               alt={social.name}
-              className="w-7 h-7 rounded-full bg-white items-center justify-center flex font-bold shadow-md "
+              className="w-6 h-6 min-h-4 min-w-4 items-center justify-center flex
+               shadow-md "
             />
           </Link>
         ))}
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,12 +1,17 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const metadata = {
+    title: 'Akshat Sharma| Portfolio',
+    description: 'Akshat Sharma Portfolio',
+  };
   return (
     <Html lang="en">
       <Head />
       <title>
         Akshat Sharma| Portfolio
       </title>
+      <meta name="description" content={metadata.description}/>
       <body>
         <Main />
         <NextScript />
